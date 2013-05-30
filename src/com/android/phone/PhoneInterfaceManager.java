49f2f935715f30b3ea4edca5439d2484baf6ac95
Marco Chiotti <marcox.chiotti@intel.com>
2013-05-30 08:55:18 +0200

    [PORT FROM MAIN] Phone: Enable the transmit of APDUs to CWS Modem Manager
    BZ: 111901
    
    Add the check of the CWS Service Manager UID (PHONE_UID) in
    exchangeIccAPDU(), openICCLogicalChannel() and
    closeIccLogicalChannel()
    
    Category: feature differentiation
    Domain: CWS-CSM
    Origin: internal
    Upstream-Candidate: no: proprietary
    Change-Id: Ifaf3551c65de39689a56286603493b7338c2df73
    Orig-Change-Id: I17f1fbe03831a8b8c2182c1711a9da2653a22bf8
    Signed-off-by: Marco Chiotti <marcox.chiotti@intel.com>

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -1031,8 +1031,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
 
     private String exchangeIccAPDU(int cla, int command,
             int channel, int p1, int p2, int p3, String data) {
-        if (Binder.getCallingUid() != Process.SMARTCARD_UID) {
-            throw new SecurityException("Only Smartcard API may access UICC");
+        if ((Binder.getCallingUid() != Process.SMARTCARD_UID)
+                && (Binder.getCallingUid() != Process.PHONE_UID)) {
+            throw new SecurityException("Only Smartcard API or CwsServiceManager may access UICC");
         }
 
         Log.d(LOG_TAG, "> exchangeAPDU " + channel + " " + cla + " " +
@@ -1062,8 +1063,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     }
 
     public int openIccLogicalChannel(String AID) {
-        if (Binder.getCallingUid() != Process.SMARTCARD_UID) {
-            throw new SecurityException("Only Smartcard API may access UICC");
+        if ((Binder.getCallingUid() != Process.SMARTCARD_UID)
+                && (Binder.getCallingUid() != Process.PHONE_UID)) {
+            throw new SecurityException("Only Smartcard API or CwsServiceManager may access UICC");
         }
 
         Log.d(LOG_TAG, "> openIccLogicalChannel " + AID);
@@ -1073,8 +1075,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     }
 
     public boolean closeIccLogicalChannel(int channel) {
-        if (Binder.getCallingUid() != Process.SMARTCARD_UID) {
-            throw new SecurityException("Only Smartcard API may access UICC");
+        if ((Binder.getCallingUid() != Process.SMARTCARD_UID)
+                && (Binder.getCallingUid() != Process.PHONE_UID)) {
+            throw new SecurityException("Only Smartcard API or CwsServiceManager may access UICC");
         }
 
         Log.d(LOG_TAG, "> closeIccLogicalChannel " + channel);
